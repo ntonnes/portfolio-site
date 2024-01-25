9f6d22be777243ff22b097116160af86f73a217d
Noah Tonnesen <noah.tonnesen@gmail.com>
2024-01-25 00:23:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// You can add JavaScript for interactivity here, such as smooth scrolling, form validation, etc.
+// Example:
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
